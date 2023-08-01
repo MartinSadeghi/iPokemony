7f9033e069460602886677f67c693ac74e58b397
moMMo Sadeghi <37694875+moMMoSadeghi@users.noreply.github.com>
2023-08-01 10:48:40 +0200

    UIImage extension added.

--- iPokemony/Extension/UIImageView + Extension.swift
+++ iPokemony/Extension/UIImageView + Extension.swift
@@ -5,4 +5,14 @@
 //  Created by Mommo Sadeghi on 24/07/23.
 //
 
-import Foundation
+import UIKit
+
+
+extension UIImageView {
+    func setImage(with urlString: String) {
+        guard let url = URL.init(string: urlString) else { return }
+        let resource = KF.ImageResource(downloadURL: url, cacheKey: urlString)
+        kf.indicatorType = .activity
+        kf.setImage(with: resource)
+    }
+}
